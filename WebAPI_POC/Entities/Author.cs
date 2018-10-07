7c6187f05f084fbf760ce8f9a9ea786e59dc07db
juan.bulfon <juan.bulfon@globallogic.com>
2018-10-07 05:56:02 -0300

    Entities were added

---
+++ WebAPI_POC/Entities/Author.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI_POC.Entities
+{
+    public class Author
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string LastName { get; set; }
+    }
+}

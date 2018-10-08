c92f9f27b29602b34538beedb64afae8d9cb2581
juan.bulfon <juan.bulfon@globallogic.com>
2018-10-08 12:14:56 -0300

    BooksContexts was added to service configurations. Migration was changed to occur only when there aren't books.

--- WebAPI_POC/Contexts/BooksContext.cs
+++ WebAPI_POC/Contexts/BooksContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using WebAPI_POC.Entities;
 
 namespace WebAPI_POC.Contexts
@@ -15,35 +16,38 @@ namespace WebAPI_POC.Contexts
 
         protected void SetInitialData()
         {
-            Books = new List<Book>
+            if (Books == null || !Books.Any())
             {
-                new Book()
+                Books = new List<Book>
                 {
-                    Title = "White Fang",
-                    Author = new Author()
+                    new Book()
                     {
-                        FirstName = "Jack",
-                        LastName = "London",
-                        Id = Guid.Parse("f42e2fdf-adcb-4207-9597-c7b5c28c9414")
+                        Title = "White Fang",
+                        Author = new Author()
+                        {
+                            FirstName = "Jack",
+                            LastName = "London",
+                            Id = Guid.Parse("f42e2fdf-adcb-4207-9597-c7b5c28c9414")
+                        },
+                        AuthorId = Guid.Parse("f42e2fdf-adcb-4207-9597-c7b5c28c9414"),
+                        Description = "",
+                        Id = Guid.Parse("65331762-e546-4c07-8fa6-9bb9e86b8fcf")
                     },
-                    AuthorId = Guid.Parse("f42e2fdf-adcb-4207-9597-c7b5c28c9414"),
-                    Description = "",
-                    Id = Guid.Parse("65331762-e546-4c07-8fa6-9bb9e86b8fcf")
-                },
-                new Book()
-                {
-                    Title = "Agile software development",
-                    Author = new Author()
+                    new Book()
                     {
-                        FirstName = "Robert C.",
-                        LastName = "Martin",
-                        Id = Guid.Parse("7f4a3fbf-bc40-4d47-a7d6-3440a9a6d6cf")
-                    },
-                    AuthorId = Guid.Parse("7f4a3fbf-bc40-4d47-a7d6-3440a9a6d6cf"),
-                    Description = "",
-                    Id = Guid.Parse("b8e4d2c6-5765-4af7-974c-50c799819927")
-                }
-            };
+                        Title = "Agile software development",
+                        Author = new Author()
+                        {
+                            FirstName = "Robert C.",
+                            LastName = "Martin",
+                            Id = Guid.Parse("7f4a3fbf-bc40-4d47-a7d6-3440a9a6d6cf")
+                        },
+                        AuthorId = Guid.Parse("7f4a3fbf-bc40-4d47-a7d6-3440a9a6d6cf"),
+                        Description = "",
+                        Id = Guid.Parse("b8e4d2c6-5765-4af7-974c-50c799819927")
+                    }
+                };
+            }
         }
     }
 }
--- WebAPI_POC/Startup.cs
+++ WebAPI_POC/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using WebAPI_POC.Contexts;
 using WebAPI_POC.Repositories;
 
 namespace WebAPI_POC
@@ -19,6 +20,7 @@ namespace WebAPI_POC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<BooksContext>();
             services.AddScoped<IBookRepository, BookRepository>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
